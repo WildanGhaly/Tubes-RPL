f5a6c2226baa7757158d7d6d04158671b21aff82
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-04-03 16:18:19 +0700

    feat: add Entity Class Quotes

---
+++ Quotes/Quotes.py
@@ -0,0 +1,15 @@
+import csv
+
+class Quotes:
+    # Konstruktor
+    def __init__(self):
+        self.filename = './Quotes.csv'
+        with open(self.filename, 'r') as file:
+            csvreader = csv.reader(file)
+            next(csvreader)  # Skip header row
+            self.quotes = [[int(row[0]), row[1]] for row in csvreader]
+    
+    # Getter
+    def get_quote(self, index):
+        return self.quotes[index][1]
+    
