72676b1ed43eae787944e135fbed3d674059e40c
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-04-06 18:39:52 +0700

    feat: add add_mood function to add mood to dbNself

--- Mood/mood_service.py
+++ Mood/mood_service.py
@@ -8,4 +8,14 @@ class Mood_Service(m.mood):
     def validate_mood(self, moods):
         return True
     
+    def add_mood(self, moods):
+        if not self.validate_mood(moods):
+            return False
+        
+        self.mood.append(self.moodToInt(moods))
+        
+        with open(self.filename, 'w', newline='') as file:
+            csv.writer(file).writerows([['ID','Joy', 'Sadness', 'Anger', 'Fear', 'Disgust', 'Surprise', 'Trust', 'Anticipation']] + self.mood)
+        
+        return True;
     
