9d58165fbee171d1a00b3f4f6ae2ab53f0563e93
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-06 18:37:23 +0700

    feat: add mood getter and tester

--- Mood/mood.py
+++ Mood/mood.py
@@ -12,4 +12,29 @@ class mood:
                           int(row[6]), int(row[7]), int(row[8])
                           ] for row in csvreader]
             
+    def moodToInt(self, row):
+        return [
+            int(row[0]), int(row[1]), int(row[2]),
+            int(row[3]), int(row[4]), int(row[5]),
+            int(row[6]), int(row[7]), int(row[8])
+        ]
     
+    # Getter
+    def get_mood(self, index):
+        allMood = []
+        for i in range (1, 9):
+            allMood.append(self.mood[index][i])
+        return allMood
+    
+    def get_mood_desc(self, index):
+        allMood = []
+        for i in range (0, 9):
+            allMood.append(self.mood[index][i])
+        return allMood
+
+    def get_specific_mood(self, index, indexMood):
+        return self.mood[index][indexMood]
+    
+    
+# moodS = mood()
+# print(moodS.get_mood(0))
