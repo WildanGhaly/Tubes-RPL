106aabd4311931a275072f1cc8140bf9a40e9988
Haikal Ardzi Shofiyyurrohman <103443120+AlphaThrone@users.noreply.github.com>
2023-04-08 12:59:15 +0700

    feat: base for chart in GUI (#9)
    * feat: Base SleepViz GUI
    
    * feat: base for chart in GUI

--- Sleep/Sleep_Visualization_GUI.py
+++ Sleep/Sleep_Visualization_GUI.py
@@ -10,6 +10,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QDateEdit, QFrame, QGraphicsView,
     QLabel, QPushButton, QSizePolicy, QWidget)
 
+from Sleep_Plot import *
+
 class Ui_Widget(QWidget):
     def __init__(self):
         
@@ -22,6 +24,8 @@ class Ui_Widget(QWidget):
         self.setupUi(self)
     
     def setupUi(self, Widget):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
         if not Widget.objectName():
             Widget.setObjectName(u"Widget")
         Widget.resize(1920, 1080)
@@ -31,68 +35,84 @@ class Ui_Widget(QWidget):
         
         self.pushButton = QPushButton(Widget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(1720, 970, 170, 80))
+        self.pushButton.setGeometry(QRect(1720, 900, 170, 80))
         
         font = QFont()
         font.setPointSize(20)
         self.pushButton.setFont(font)
         self.dateEdit = QDateEdit(Widget)
         self.dateEdit.setObjectName(u"dateEdit")
-        self.dateEdit.setGeometry(QRect(400, 970, 171, 41))
+        self.dateEdit.setGeometry(QRect(400, 915, 171, 41))
         self.pilihTanggalText = QLabel(Widget)
         self.pilihTanggalText.setObjectName(u"pilihTanggalText")
-        self.pilihTanggalText.setGeometry(QRect(220, 960, 371, 61))
+        self.pilihTanggalText.setGeometry(QRect(220, 900, 371, 61))
         self.pilihTanggalText.setFont(font)
         
         #BackGround
-        self.background = QGraphicsView(Widget)
-        self.background.setObjectName(u"background")
-        self.background.setGeometry(QRect(0, 0, 1920, 1080))
-        self.background.setMinimumSize(QSize(1920, 1080))
-        self.background.setMaximumSize(QSize(1920, 1080))
+        # self.background = QGraphicsView(Widget)
+        # self.background.setObjectName(u"background")
+        # self.background.setGeometry(QRect(0, 0, 1920, 1080))
+        # self.background.setMinimumSize(QSize(1920, 1080))
+        # self.background.setMaximumSize(QSize(1920, 1080))
+        
+        #Judul
         self.JudulText = QLabel(Widget)
         self.JudulText.setObjectName(u"JudulText")
         self.JudulText.setGeometry(QRect(100, 30, 1691, 71))
         self.JudulText.setAlignment(Qt.AlignCenter)
         self.JudulText.setFont(font)
-        self.frame = QFrame(Widget)
-        self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(20, 250, 600, 600))
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
+        
+        #Start Sleep Chart
+        startSleepChart = PlotCanvas(self, width=5, height=4)
+        startSleepChart.move(20,250)
+        # self.frame = QFrame(startSleepChart)
+        # self.frame.setObjectName(u"frame")
+        # self.frame.setGeometry(QRect(20, 250, 600, 600))
+        # self.frame.setFrameShape(QFrame.StyledPanel)
+        # self.frame.setFrameShadow(QFrame.Raised)
         self.startSleepChart = QLabel(Widget)
         self.startSleepChart.setObjectName(u"startSleepChart")
         self.startSleepChart.setGeometry(QRect(20, 180, 601, 71))
         self.startSleepChart.setAlignment(Qt.AlignCenter)
+        
+        #Duration Sleep Chart
+        durationSleep = PlotCanvas(self, width=5, height=4)
+        durationSleep.move(650,250)
         self.durationSleepChart = QLabel(Widget)
         self.durationSleepChart.setObjectName(u"durationSleepChart")
         self.durationSleepChart.setGeometry(QRect(650, 180, 601, 71))
         self.durationSleepChart.setAlignment(Qt.AlignCenter)
-        self.frame_2 = QFrame(Widget)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setGeometry(QRect(650, 250, 600, 600))
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
+        # self.frame_2 = QFrame(Widget)
+        # self.frame_2.setObjectName(u"frame_2")
+        # self.frame_2.setGeometry(QRect(650, 250, 600, 600))
+        # self.frame_2.setFrameShape(QFrame.StyledPanel)
+        # self.frame_2.setFrameShadow(QFrame.Raised)
+        
+        #endSleepChart
         self.endSleepChart = QLabel(Widget)
         self.endSleepChart.setObjectName(u"endSleepChart")
         self.endSleepChart.setGeometry(QRect(1270, 180, 601, 71))
         self.endSleepChart.setAlignment(Qt.AlignCenter)
-        self.frame_3 = QFrame(Widget)
-        self.frame_3.setObjectName(u"frame_3")
-        self.frame_3.setGeometry(QRect(1270, 250, 600, 600))
-        self.frame_3.setFrameShape(QFrame.StyledPanel)
-        self.frame_3.setFrameShadow(QFrame.Raised)
-        self.background.raise_()
+        endSleepChart = PlotCanvas(self, width=5, height=4)
+        endSleepChart.move(1270,250)
+        # self.frame_3 = QFrame(Widget)
+        # self.frame_3.setObjectName(u"frame_3")
+        # self.frame_3.setGeometry(QRect(1270, 250, 600, 600))
+        # self.frame_3.setFrameShape(QFrame.StyledPanel)
+        # self.frame_3.setFrameShadow(QFrame.Raised)
+        
+        
+        # self.background.raise_()
         self.pilihTanggalText.raise_()
         self.pushButton.raise_()
         self.dateEdit.raise_()
         self.JudulText.raise_()
-        self.frame.raise_()
+        # self.frame.raise_()
         self.startSleepChart.raise_()
         self.durationSleepChart.raise_()
-        self.frame_2.raise_()
+        # self.frame_2.raise_()
         self.endSleepChart.raise_()
-        self.frame_3.raise_()
+        # self.frame_3.raise_()
 
         self.retranslateUi(Widget)
 
