349426f7de3a2cceff5bf64ef561425b693be8cd
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-04-08 11:45:43 +0700

    feat: add Mood_Form using pyqt

---
+++ Mood/mood_pyqt.py
@@ -0,0 +1,25 @@
+import resource_rc
+import ui_mood_input_rev1 as uw 
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout,QSizePolicy
+import sys
+from PySide6 import QtWidgets
+from datetime import date, datetime
+from mood_service import Mood_Service
+
+class Mood_Form(QWidget, uw.Ui_Form, Mood_Service):
+    def __init__(self):
+        super().__init__()
+        Mood_Service.__init__(self)
+        self.setupUi(self)
+        self.submitButtonMood.clicked.connect(self.do_something)
+        
+    
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = Mood_Form()
+window.show()
+
+app.exec()
