538bb827d33b85e526328aec74183b8d8181681a
AlphaThrone <haikalardzi@gmail.com>
2023-04-15 16:49:37 +0700

    add: base sleep recommendation

--- Sleep/Sleep_Recommendation.py
+++ Sleep/Sleep_Recommendation.py
@@ -42,21 +42,8 @@ class Ui_Widget(QWidget):
         self.sleepViz.setSizePolicy(sizePolicy)
         self.sleepViz.setMinimumSize(QSize(1366, 720))
         self.sleepViz.setMaximumSize(QSize(1366, 720))
-        self.sleepViz.setStyleSheet(u"background-image: url(:/newPrefix/vstock/sleep time input fg.png)")
-        self.plainTextEdit = QPlainTextEdit(Widget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(680, 244, 275, 50))
-        self.plainTextEdit = QPlainTextEdit(Widget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(680, 344, 275, 50))
-        self.plainTextEdit = QPlainTextEdit(Widget)
-        self.plainTextEdit.setObjectName(u"plainTextEdit")
-        self.plainTextEdit.setGeometry(QRect(730, 444, 225, 50))
+        self.sleepViz.setStyleSheet(u"background-image: url(:/newPrefix/vstock/sleep recommendation fg.png)")
         
-        self.nextButton = QPushButton(Widget)
-        self.nextButton.setObjectName(u"nextButton")
-        self.nextButton.setGeometry(QRect(600, 547, 170, 72))
-        self.nextButton.setStyleSheet("QPushButton{background: transparent;}")
         self.retranslateUi(Widget)
 
         QMetaObject.connectSlotsByName(Widget)
@@ -66,7 +53,7 @@ class Ui_Widget(QWidget):
 
     def retranslateUi(self, Widget):
         Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
-        self.nextButton.setText(QCoreApplication.translate("Widget", u"", None))
+        # self.nextButton.setText(QCoreApplication.translate("Widget", u"", None))
         self.sleepViz.setText("")
     # retranslateUi
     
