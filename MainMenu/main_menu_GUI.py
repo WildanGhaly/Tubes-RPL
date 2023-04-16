aa303b2b2a8ae804fa444127df39754963c94126
Hidayatullah Wildan Ghaly Buchary <110533432+WildanGhaly@users.noreply.github.com>
2023-04-16 23:45:35 +0700

    Integrasi Mood (#26)
    * feat: add callable history GUI
    
    * fix: rombak history
    
    * feat: add MAIN PROGRAM
    
    * feat: add callable main menu GUI
    
    * fix: rombak main menu
    
    * fix: rombak mood prediction
    
    * fix: rombak mood visual calendar
    
    * fix: update path for mood visual
    
    * fix: fix mood input error after integrate
    
    * fix: update mood service
    
    * fix: update path for mood
    
    * fix: update all UI for MOOD

--- MainMenu/main_menu_GUI.py
+++ MainMenu/main_menu_GUI.py
@@ -1,16 +1,18 @@
-import Mood.resource_rc
-import Mood.ui_mood_visual_calendar as uw 
+import MainMenu.mainmenu as uw 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout,QSizePolicy
 import sys
 from PyQt5 import QtWidgets
 from datetime import date, datetime
-from Mood.mood_service import Mood_Service
 
-class Mood_Form_Calendar(QWidget, uw.Ui_Dialog):
-    
+class Main_Menu_GUI(QWidget, uw.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
     
-        
+# app = QtWidgets.QApplication(sys.argv)
+
+# window = Main_Menu_GUI()
+# window.show()
+
+# sys.exit(app.exec_())
