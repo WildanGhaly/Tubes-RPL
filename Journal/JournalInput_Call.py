590bf7a68380d46e75dad095e749af910cb330f0
christodharma <13521009@std.stei.itb.ac.id>
2023-04-19 16:18:53 +0700

    WIP: Change UI, Background, File relocations

--- Journal/JournalInput_Call.py
+++ Journal/JournalInput_Call.py
@@ -5,13 +5,28 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout,QSizePoli
 import sys
 from PyQt5 import QtWidgets
 from datetime import date, datetime
-
+import Journal.JournalDatabase as JournalDatabase
+from tkinter import messagebox
+from Journal.Journal import Journal as jrn
 class Journal_GUI_Call(QWidget, uw.JournalInput):
     
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        DB = JournalDatabase.JournalDatabase()
+        self.SubmitButton.clicked.connect(self.addToJournalDB)
+    def addToJournalDB(self):
+        if self.JournalContent.toPlainText().strip()=="":
+        # do nothing
+                messagebox.showinfo("Failed", "Konten Jurnal kosong.")
+        else : 
+                addedContent = jrn([jrn.DatetimeToID(),self.JournalContent.toPlainText(),"Title"+jrn.DatetimeToID()])
+                self.DB.addJournal(addedContent.toCSV())
+                messagebox.showinfo("Success", "Konten Jurnal ditambahkan.")
+                from MainMenu.main_menu_GUI import Main_Menu_GUI
+                self.main_menu = Main_Menu_GUI()
+                self.main_menu.show()
+                self.hide()
 # app = QtWidgets.QApplication(sys.argv)
 
 # window = Journal_GUI_Call()
